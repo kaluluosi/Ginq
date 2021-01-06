51cd4baa7a40bee3d10689294507620f2eb8921f
dengxuan <303359166@qq.com>
2021-01-06 19:12:15 +0800

    create project

---
+++ script/Ginq.gd
@@ -0,0 +1,443 @@
+extends Object
+class_name Ginq
+
+var array: Array setget ,getArray
+var _operators = []
+var _lambdas = []
+var host:Object
+
+func _init(iterable: Array, operators:=[], lambdas:=[]):
+	array = iterable.duplicate(true)
+	_operators = operators
+	_lambdas = lambdas
+	
+func getArray():
+	return array
+	
+func val():
+	_init_lambda_host() 
+	
+	var tempValue = array
+	
+	# 处理链式操作
+	for operator in _operators:
+		tempValue = call(operator.method, operator.args, tempValue)
+	
+	return tempValue
+	
+# region
+
+
+func _new_ginq(iterable:Array, operators:=[], lambdas:=[]) -> Ginq:
+	return load(self.get_script().get_path()).new(iterable, operators, lambdas)
+
+func _clone() -> Ginq:
+	return _new_ginq(array, _operators.duplicate(true), _lambdas.duplicate(true))	
+
+static func eval(code:String):
+	var script = GDScript.new()
+	script.set_source_code("func eval():\n\treturn "+code)
+	script.reload()
+	var obj = Reference.new()
+	obj.set_script(script)
+	var ret = obj.eval()
+	return ret
+
+static func is_lambda(code:String) -> bool:
+	return code.find('lambda') > 0
+	
+func add_lambda(code:String) -> String:
+	var lambda_format = """func {func_name}({args}):\n\treturn {express}"""
+		
+	var regex = RegEx.new()
+	regex.compile("lambda(?<args>[\\w, ]+):(?<express>.+)")
+	var result = regex.search(code)
+	if result:
+		var argsStr = result.get_string('args')
+		var express = result.get_string('express')
+		var name = 'lambda_{func_name}'.format({func_name=v4('_')})
+		var lambda_code = lambda_format.format({args=argsStr, express=express, func_name=name})
+		_lambdas.append(lambda_code)
+		return name
+	else:
+		return 'error'
+
+func _init_lambda_host():
+	var lambda_codes = PoolStringArray(_lambdas).join('\n')
+
+	var script = GDScript.new()
+	script.set_source_code(lambda_codes)
+	script.reload()
+	var hostObj = Reference.new()
+	hostObj.set_script(script)
+	host = hostObj
+	
+func register_operator(method:String, args):
+	_operators.append({method=method, args=args})
+		
+func filter(lambda: String) -> Ginq:
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_filter', {lambda_name=lambda_name})
+	return clone
+
+func _filter(args, iterable: Array) -> Array:
+	var ret:Array = []
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		for v in iterable:
+			if host.call(lambda_name, v):
+				ret.push_back(v)
+		return ret
+	else:
+		push_error('lambda not found')
+		return []
+		
+func where(lambda:String) -> Ginq:
+	"""
+	alias of filter
+	"""
+	return filter(lambda)
+	
+func map(lambda: String) -> Ginq:
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_map', {lambda_name=lambda_name})
+	return clone
+	
+func _map(args, iterable:Array) -> Array:
+	var ret:Array = []
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		for v in iterable:
+			var nv = host.call(lambda_name, v)
+			ret.push_back(nv)
+		return ret
+	else:
+		push_error('lambda not found')
+		return []
+		
+func select(lambda:String) -> Ginq:
+	"""
+	alias of map
+	"""
+	return map(lambda)
+	
+func skip(num:int) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_skip', {num=num})
+	return clone
+	
+func _skip(args, iterable: Array):
+	var num = args.num
+	var skipnum = num if num - 1 > 0 else 0
+	return iterable.slice(skipnum, len(iterable)-1)
+
+func skip_while(lambda:String) -> Ginq:
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_skip_while',{lambda_name=lambda_name})
+	return clone
+
+func _skip_while(args, iterable:Array) -> Array:
+	var start_index = 0
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		for index in range(len(iterable)):
+			if host.call(lambda_name, iterable[index]):
+				start_index+=1
+			else:
+				break
+		return iterable.slice(start_index, len(iterable)-1)
+	else:
+		push_error("lambda not found")
+		return []
+
+func take(num:int) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_take', {num=num})
+	return clone
+	
+func _take(args, iterable:Array) -> Array:
+	var num = args.num
+	if num <= 0:
+		return []
+	elif num > len(iterable):
+		return iterable
+	else:
+		return iterable.slice(0, num-1)
+		
+func take_while(lambda:String):
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_take_while', {lambda_name=lambda_name})
+	return clone
+
+func _take_while(args, iterable:Array) -> Array:
+	var end_index = -1
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		for index in len(iterable):
+			if host.call(lambda_name, iterable[index]):
+				end_index += 1
+			else:
+				break
+		if end_index == -1:
+			return []
+		else:
+			return iterable.slice(0, end_index)
+	else:
+		push_error('lambda not found')
+		return []
+
+func join(secendIterable:Array, lambda_source_key:String, lambda_inner_key:String) -> Ginq:
+	var clone = _clone()
+	var lambda_source_key_name = clone.add_lambda(lambda_source_key)
+	var lambda_inner_key_name = clone.add_lambda(lambda_inner_key)
+	clone.register_operator('_join', {lambda_source_key_name=lambda_source_key_name, lambda_inner_key_name=lambda_inner_key_name, secendIterable=secendIterable})
+	return clone
+
+func _join(args, iterable:Array) -> Array:
+	var ret = []
+	if host.has_method(args.lambda_source_key_name) and host.has_method(args.lambda_inner_key_name):
+		for v in iterable:
+			var source_key_value = host.call(args.lambda_source_key_name, v)
+			for inner in args.secendIterable:
+				var inner_key_value = host.call(args.lambda_inner_key_name, inner)
+				if source_key_value == inner_key_value:
+					ret.push_back([v, inner])
+		return ret
+	else:
+		push_error('lambda not found')
+		return []
+
+func concate(iterable:Array) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_concate', {iterable=iterable})
+	return clone
+
+func _concate(args, iterable:Array) -> Array:
+	var target_iterable:Array = args.iterable
+	var ret = iterable.duplicate(true)
+	for v in target_iterable:
+		ret.push_back(v)
+	return ret
+
+func order_by(lambda:String = "lambda x:x") -> Ginq:
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_order_by', {lambda_name=lambda_name})
+	return clone
+
+func _order_by(args, iterable:Array) -> Array:
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		var ret = []
+		var temp = {}
+		for v in iterable:
+			var key = host.call(lambda_name, v)
+			temp[key] = v
+
+		var sorted_keys = temp.keys()
+		sorted_keys.sort()
+		for key in sorted_keys:
+			ret.push_back(temp[key])
+		
+		return ret
+	else:
+		push_error('lambda not found')
+		return iterable
+
+func order_by_descending(lambda:String="lambda x:x") -> Ginq:
+	var clone = _clone()
+	var lambda_name = clone.add_lambda(lambda)
+	clone.register_operator('_order_by_descending', {lambda_name=lambda_name})
+	return clone
+
+func _order_by_descending(args, iterable:Array) -> Array:
+	var lambda_name = args.lambda_name
+	if host.has_method(lambda_name):
+		var ret = []
+		var temp = {}
+		for v in iterable:
+			var key = host.call(lambda_name, v)
+			temp[key] = v
+
+		var sorted_keys = temp.keys()
+		sorted_keys.sort()
+		var end_index = len(sorted_keys) -1
+		while end_index >=0:
+			var key = sorted_keys[end_index]
+			ret.push_back(temp[key])
+			end_index-=1
+		
+		return ret
+	else:
+		push_error('lambda not found')
+		return iterable
+
+func reverse() -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_reverse', null)
+	return clone
+
+func _reverse(args, iterable:Array) -> Array:
+	var ret = []
+	for v in iterable:
+		ret.push_front(v)
+	return ret
+
+func distinct() -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_distinct', {})
+	return clone
+
+func _distinct(args, iterable:Array) -> Array:
+	var ret = []
+	for v in iterable:
+		if v in ret:
+			continue
+		else:
+			ret.push_back(v)
+	return ret
+
+func union(secendIterable:Array) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_union', {secendIterable=secendIterable})
+	return clone
+
+func _union(args, iterable:Array) -> Array:
+	var ret = _new_ginq(iterable).concate(args.secendIterable).distinct().val()
+	return ret
+
+func intersect(secendIterable:Array) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_intersect', {secendIterable=secendIterable})
+	return clone
+
+func _intersect(args, iterable:Array) -> Array:
+	var ret = []
+	for v in iterable:
+		if v in args.secendIterable:
+			ret.push_back(v)
+	return ret
+
+func expect(secendIterable:Array) -> Ginq:
+	var clone = _clone()
+	clone.register_operator('_expect', {secendIterable=secendIterable})
+	return clone	
+
+func _expect(args, iterable:Array) -> Array:
+	var ret = []
+	iterable = _new_ginq(iterable).concate(args.secendIterable).val()
+	for v in iterable:
+		if v in ret:
+			var index = ret.find(v)
+			ret.remove(index)
+			continue
+		else:
+			ret.push_back(v)
+		
+	return ret
+
+func all(lambda:String="lambda x:x") -> bool:
+	var temp_array = map(lambda).val()
+	var ret = true
+	for value in temp_array:
+		ret = ret and value
+		if not ret:
+			return false
+	return true
+
+func any(lambda: String="lambda x:x") -> bool:
+	var temp_array = map(lambda).val()
+	var ret = false
+	for value in temp_array:
+		ret = ret or value
+		if ret:
+			return true
+	return false
+
+func sum(lambda:String="lambda x:x"):
+	var temp_array = map(lambda).val()
+	var ret = 0
+	for value in temp_array:
+		if typeof(value) in [TYPE_INT,TYPE_REAL]:
+			ret += value
+		else:
+			push_error('{} is not number'.format({0:value}))
+			return -1
+
+	return ret
+
+func min(lambda:String="lambda x:x"):
+	var temp_array = map(lambda).val()
+	
+	var min_value = temp_array.pop_front()
+	for value in temp_array:
+		if value < min_value:
+			min_value = value
+
+	return min_value
+
+func max(lambda:String="lambda x:x"):
+	var temp_array = map(lambda).val()
+
+	var max_value = temp_array.pop_front()
+	for value in temp_array:
+		if value > max_value:
+			max_value = value
+
+	return max_value
+
+func average(lambda:String="lambda x:x"):
+	var temp_array = map(lambda).val()
+	var tempGinq = _new_ginq(temp_array)
+	var sum = tempGinq.sum()
+	return sum/len(temp_array)
+
+# end region
+
+"""
+thanks for Xavier Sellier
+https://github.com/binogure-studio/godot-uuid
+"""
+
+const MODULO_8_BIT = 256
+
+static func getRandomInt():
+  # Randomize every time to minimize the risk of collisions
+  randomize()
+
+  return randi() % MODULO_8_BIT
+
+static func uuidbin():
+  # 16 random bytes with the bytes on index 6 and 8 modified
+  return [
+	getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(),
+	getRandomInt(), getRandomInt(), ((getRandomInt()) & 0x0f) | 0x40, getRandomInt(),
+	((getRandomInt()) & 0x3f) | 0x80, getRandomInt(), getRandomInt(), getRandomInt(),
+	getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(),
+  ]
+
+static func v4(delimiter='-'):
+  # 16 random bytes with the bytes on index 6 and 8 modified
+  var b = uuidbin()
+
+  return ('%02x%02x%02x%02x{delimiter}%02x%02x{delimiter}%02x%02x{delimiter}%02x%02x{delimiter}%02x%02x%02x%02x%02x%02x' % [
+	# low
+	b[0], b[1], b[2], b[3],
+
+	# mid
+	b[4], b[5],
+
+	# hi
+	b[6], b[7],
+
+	# clock
+	b[8], b[9],
+
+	# clock
+	b[10], b[11], b[12], b[13], b[14], b[15]
+  ]).format({delimiter=delimiter})
+
---
+++ test/unit/test_ginq.gd
@@ -0,0 +1,330 @@
+extends "res://addons/gut/test.gd"
+
+var L = load('res://script/Ginq.gd')
+
+func test_filter():
+	var l:Ginq = L.new([1,1,2,3])
+	var ret = l.filter("lambda x: x==1").val()
+	assert_eq(ret, [1,1], 'not match')
+
+func test_map():
+	var l:Ginq = L.new([1,1,2,3])
+	var ret = l.map("lambda x:x*2").val()
+	assert_eq(ret, [2,2,4,6], 'not match')
+
+func _skip_params():
+	var params = [
+		['array', 'skip', 'expect'],
+		[
+			[[1,1,2,3],2,[2,3]],
+			[[1,1,2,3],1,[1,1,2,3]],
+			[[1,1,2,3],0,[1,1,2,3]],
+			[[1,1,2,3],5,[]]
+		]
+	]
+
+	return ParameterFactory.named_parameters(params[0], params[1])
+
+func test_skip(params=use_parameters(_skip_params())):
+	var l:Ginq = L.new(params.array)
+	var ret = l.skip(params.skip).val()
+	assert_eq(ret, params.expect, 'not match')
+
+func _skip_while_params():
+	var params = [
+		['array', 'lambda', 'expect'],
+		[
+			[[1,1,2,3], "lambda x:x<2", [2,3]],
+			[[1,1,2,3], "lambda x:x>2", [1,1,2,3]],
+		]
+	]
+	return ParameterFactory.named_parameters(params[0], params[1])
+
+func test_skip_while(params=use_parameters(_skip_while_params())):
+	var l:Ginq = L.new(params.array)
+	var ret = l.skip_while(params.lambda).val()
+	assert_eq(ret, params.expect, 'not match')
+
+
+func _take_params():
+	var params = [
+		['array', 'take', 'expect'],
+		[
+			[[1,1,2,3], 2, [1,1]],
+			[[1,1,2,3], 0, []],
+			[[1,1,2,3], 5, [1,1,2,3]]
+		]
+	]
+	return ParameterFactory.named_parameters(params[0], params[1])
+
+
+func test_take(params=use_parameters(_take_params())):
+	var l:Ginq = L.new(params.array)
+	var ret = l.take(params.take).val()
+	assert_eq(ret, params.expect, 'not match')
+
+
+func _take_while_params():
+	var params = [
+		['array', 'lambda', 'expect'],
+		[
+			[[1,1,2,3], 'lambda x:x<2', [1,1]],
+			[[1,1,2,3], 'lambda x:x>2', []],
+			[[1,1,2,3], 'lambda x:x>0', [1,1,2,3]],
+		]
+	]
+
+	return ParameterFactory.named_parameters(params[0], params[1])
+
+func test_take_while(params=use_parameters(_take_while_params())):
+	var l:Ginq = L.new(params.array)
+	var ret = l.take_while(params.lambda).val()
+	assert_eq(ret, params.expect, 'not match')
+
+
+func test_join():
+	var left = [
+		{id=1, name='a'}, {id=2,name='b'}
+	]
+	var right = [
+		{id=1, age=1}, {id=2,age=2}
+	]
+
+	var left_ginq:Ginq = L.new(left)
+	var ret = left_ginq.join(right, "lambda x:x.id", "lambda y:y.id").val()
+
+	assert_eq(ret[0][0].id, ret[0][1].id, 'not match')
+	assert_eq(ret[1][0].id, ret[1][1].id, 'not match')
+
+	ret = left_ginq.join(right, "lambda x:x.id", "lambda y:y.id").select("lambda pair: {id=pair[0].id, name=pair[0].name, age=pair[1].age}").val()
+	assert_eq(ret[0].id,1, 'not match')
+	assert_eq(ret[0].name,'a', 'not match')
+	assert_eq(ret[0].age,1, 'not match')
+
+
+func test_concate():
+	var left = [1,2,3]
+	var right = [4,5,6]
+
+	var left_ginq = L.new(left)
+	var ret = left_ginq.concate(right).val()
+	assert_eq(ret, [1,2,3,4,5,6], 'not match')
+
+
+func test_order():
+	var unorder = [2,3,1,4,5]
+	var order_ginq = L.new(unorder)
+	var ret = order_ginq.order_by().val()
+	assert_eq(ret, [1,2,3,4,5], 'not match')
+
+func test_order_by_lambda():
+	var unorder_obj_list = [
+		{id=2, name='b'},
+		{id=1, name='a'},
+		{id=4, name='c'},
+		{id=3, name='d'},
+	]
+
+	var order_obj_list = L.new(unorder_obj_list)
+	var ordered_obj_list = order_obj_list.order_by("lambda x:x.name").val()
+	var name_list = L.new(ordered_obj_list).map("lambda x:x.name").val()
+	assert_eq(name_list, ['a','b','c','d'], 'not match')
+
+	ordered_obj_list = order_obj_list.order_by("lambda x:x.id").val()
+	var id_list = L.new(ordered_obj_list).map("lambda x:x.id").val()
+	assert_eq(id_list, [1,2,3,4], "not match")
+	
+func test_order_descending():
+	var unorder = [2,3,1,4,5]
+	var order_ginq = L.new(unorder)
+	var ret = order_ginq.order_by_descending().val()
+	assert_eq(ret, [5,4,3,2,1], 'not match')
+
+func test_order_by_lambda_descending():
+	var unorder_obj_list = [
+		{id=2, name='b'},
+		{id=1, name='a'},
+		{id=4, name='c'},
+		{id=3, name='d'},
+	]
+
+	var order_obj_list = L.new(unorder_obj_list)
+	var ordered_obj_list = order_obj_list.order_by_descending("lambda x:x.name").val()
+	var name_list = L.new(ordered_obj_list).map("lambda x:x.name").val()
+	assert_eq(name_list, ['d','c','b','a'], 'not match')
+
+	ordered_obj_list = order_obj_list.order_by_descending("lambda x:x.id").val()
+	var id_list = L.new(ordered_obj_list).map("lambda x:x.id").val()
+	assert_eq(id_list, [4,3,2,1], "not match")
+	
+func test_reverse():
+	var array = [1,2,3,4,5]
+	var ret = L.new(array).reverse().val()
+	assert_eq(ret, [5,4,3,2,1],'not match')
+
+func test_distinct():
+	var array = [1,2,3,3,4,5,6,5]
+	var ret = L.new(array).distinct().val()
+	assert_eq(ret, [1,2,3,4,5,6], 'not match')
+
+
+func test_union():
+	var left = [1,2,3]
+	var right = [3,4,5]
+
+	var left_ginq = L.new(left)
+	var ret = left_ginq.union(right).val()
+	assert_eq(ret, [1,2,3,4,5], 'not match')
+
+func test_intersect():
+	var left = [1,2,3]
+	var right = [3,4,5]
+
+	var left_ginq = L.new(left)
+	var ret = left_ginq.intersect(right).val()
+	assert_eq(ret, [3], 'not match')
+
+func test_expect():
+	var left = [1,2,3]
+	var right = [3,4,5]
+
+	var left_ginq = L.new(left)
+	var ret = left_ginq.expect(right).val()
+	assert_eq(ret, [1,2,4,5], 'not match')
+
+
+func test_all():
+	var array = [1,2,3,4]
+	var ginq = L.new(array)
+
+	var ret = ginq.all()
+	assert_eq(ret, true, 'not match')
+
+	array = [1,2,null, 4]
+	ginq = L.new(array)
+
+	ret = ginq.all()
+	assert_eq(ret, false, 'not match')
+
+	array = [
+			{
+				id=1,
+				name='a'
+			},
+			{
+				id=2,
+				name=null
+			}
+		]
+	ginq = L.new(array)
+
+	ret = ginq.all("lambda x: x.name")
+	assert_eq(ret, false, 'not match')
+
+func test_any():
+	var array = [1,2,null,4]
+	var ginq = L.new(array)
+
+	var ret = ginq.any()
+	assert_eq(ret, true, 'not match')
+
+	array = [null, null]
+	ginq = L.new(array)
+
+	ret = ginq.any()
+	assert_eq(ret, false, 'not match')
+
+	array = [
+			{
+				id=1,
+				name='a'
+			},
+			{
+				id=2,
+				name=null
+			}
+		]
+	ginq = L.new(array)
+
+	ret = ginq.any("lambda x: x.name")
+	assert_eq(ret, true, 'not match')
+
+func test_sum():
+	var array = [1,1,1,1]
+	var ginq = L.new(array)
+	var ret = ginq.sum()
+	assert_eq(ret, 4)
+
+	array = [
+		{
+			age=1
+		},
+		{
+			age=2
+		}
+	]
+
+	ginq = L.new(array)
+	ret = ginq.sum("lambda x:x.age")
+	assert_eq(ret, 3)
+
+func test_min():
+	var array = [1,2,3,4]
+	var ginq = L.new(array)
+	var ret = ginq.min()
+	assert_eq(ret, 1)
+
+	array = [
+		{
+			age=1
+		},
+		{
+			age=2
+		}
+	]
+
+	ginq = L.new(array)
+	ret = ginq.min("lambda x:x.age")
+	assert_eq(ret, 1)
+
+
+func test_max():
+	var array = [1,2,3,4]
+	var ginq = L.new(array)
+	var ret = ginq.max()
+	assert_eq(ret, 4)
+
+	array = [
+		{
+			age=1
+		},
+		{
+			age=2
+		}
+	]
+
+	ginq = L.new(array)
+	ret = ginq.max("lambda x:x.age")
+	assert_eq(ret, 2)
+
+func test_average():
+	var array =  [1,2,3]
+	var ginq = L.new(array)
+	var ret = ginq.average()
+	assert_eq(ret, 2)
+
+	array = [
+		{
+			age=1
+		},
+		{
+			age=2
+		},
+		{
+			age=3
+		}
+	]
+
+	ginq = L.new(array)
+	ret = ginq.average("lambda x:x.age")
+	assert_eq(ret, 2)
